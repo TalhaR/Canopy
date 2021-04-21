ff7ba74e28e113c3e6e83b3a564385d6171e9b9c
TalhaR <delta.talha@gmail.com>
2021-04-20 21:41:25 -0400

    adjusted css

--- client/src/components/Graph.jsx
+++ client/src/components/Graph.jsx
@@ -3,21 +3,28 @@ import React from "react";
 
 const useStyles = makeStyles({
     root: {
-        height: "400px",
-        paddingLeft: "15px"
+        height: "450px",
+        paddingLeft: "15px",
+        paddingTop: "15px",
+    },
+    title: {
+        fontSize: 36,
+        paddingBottom: "5px"
     },
     value: {
         fontSize: 24,
-        paddingTop: "15px"
     },
     change: {}
 });
 
-function Graph() {
+function Graph({ title, value, increase }) {
     const classes = useStyles();
 
     return (
         <Card className={classes.root} variant="outlined">
+            {title != null && <Typography className={classes.title} variant="h4" component="h2">
+                { title }
+            </Typography>}
             <Typography className={classes.value} variant="h5" component="h2">
                 $1234.56
             </Typography>
