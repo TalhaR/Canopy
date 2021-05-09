05c8b31fb97170db27bd649b6e2b53e857cc6545
TalhaR <delta.talha@gmail.com>
2021-05-09 12:30:47 -0400

    removed hyperlink to temp route

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,7 +1,6 @@
 import { Grid } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import React from "react";
-import { Link } from "react-router-dom";
 import Graph from "../components/Graph";
 import News from "../components/News";
 import StockList from "../components/StockList";
@@ -13,31 +12,36 @@ const useStyles = makeStyles((theme) => ({
     },
 }));
 
-function HomePage() {
+const HomePage = () => {
     const classes = useStyles();
 
     return (
         <main className={classes.root}>
             <Grid container justify="center" spacing={3}>
                 <Grid item xs={12} md={6}>
-                    <Graph />
+                    <Graph value={1234.56} />
                 </Grid>
 
                 <Grid item xs={12} sm={6} md={4}>
-                    <StockList title="Portfolio" />
+                    <StockList
+                        title="Portfolio"
+                        stockList={["VOO", "MSFT", "SPY"]}
+                    />
                 </Grid>
 
                 <Grid item xs={12} sm={6} md={4}>
-                    <StockList title="Watchlist" />
+                    <StockList
+                        title="Watchlist"
+                        stockList={["GME", "COIN", "RBLX"]}
+                    />
                 </Grid>
 
                 <Grid item xs={12} md={6}>
                     <News />
                 </Grid>
             </Grid>
-            <Link to="/stocks/msft"> Stock Page </Link>
         </main>
     );
-}
+};
 
 export default HomePage;
