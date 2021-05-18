f6cae767d19f0b13b802e1ed75a5239cd9097510
TalhaR <delta.talha@gmail.com>
2021-05-18 00:39:15 -0400

    updating graph based on query to backend

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -15,35 +15,54 @@ const useStyles = makeStyles((theme) => ({
 
 const HomePage = () => {
     const classes = useStyles();
-    const [holdings, setHoldings] = useState(["VOO", "SPY", "MSFT"]);
-    const [watchList, setWatchList] = useState(["GME", "RBLX", "COIN"]);
+    const [holdings, setHoldings] = useState([]);
+    const [watchList, setWatchList] = useState([]);
+    const [portfolioValue, setPortfolioValue] = useState(0)
+    const [history, setHistory] = useState([])   
 
     useEffect(() => {
         const getHoldings = async () => {
             let res = await axios.get("http://localhost:8080/api/holdings/user/1")
             if (res.status === 200) {
                 setHoldings(res.data.map((s) => s.ticker))
+            } else {
+                console.log(res.data);
             }
-            console.log(res.data);
         }
 
         const getWatchList = async () => {
-            let res = await axios.get("http://localhost:8080/api/watchlist/user/1")
+            let res = await axios.get("http://localhost:8080/api/watchlists/1")
             if (res.status === 200) {
-                setWatchList(res.data.map((s) => s.ticker))
+                setWatchList(res.data["Watchlist"])
+            } else {
+                console.log(res.data);
             }
-            console.log(res.data);
+        }
+
+        const getPortfolioValues = async () => {
+            let res1 = await axios.get("http://localhost:8080/api/portfolios/1")
+            let res2 = await axios.get("http://localhost:8080/api/portfolio-histories/1")
+            if (res1.status === 200) {
+                setPortfolioValue((Math.round(res1.data["netWorth"] * 100) / 100).toFixed(2))
+                setHistory(res2.data);
+            } else {
+                console.log(res1.data, res2.data);
+            }
         }
 
         getHoldings();
-        // getWatchList();
+        getWatchList();
+        getPortfolioValues()
     }, [])
 
     return (
         <main className={classes.root}>
             <Grid container justify="center" spacing={3}>
                 <Grid item xs={12} md={6}>
-                    <Graph value={1234.56} />
+                    <Graph 
+                        value={portfolioValue} 
+                        history={history} 
+                    />
                 </Grid>
 
                 <Grid item xs={12} sm={6} md={4}>
