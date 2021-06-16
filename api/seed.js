0f0ff39ba1471a90d306ae430e5c2db08f0b70e9
Kevin <Kevin.FNCW@gmail.com>
2021-06-15 23:35:53 -0400

    keep tables

--- api/seed.js
+++ api/seed.js
@@ -135,7 +135,7 @@ const HISTORIES = [
 ];
 
 const seed = () => {
-  return db.sequelize.sync({force: true})
+  return db.sequelize.sync({force: false})
     .then(() => {
       // Create all the entries
       let userPromises = USERS.map(u => User.create(u));
